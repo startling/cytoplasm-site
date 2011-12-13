2860da6fc4d47c547d42e99e5dc2dae3507e2745
tim <tdixon51793@gmail.com>
2011-12-13 15:53:53 -0600

    Configuration for controllers.
    The blog controller hasn't been committed yet, though; give it a minute.

--- _config.py
+++ _config.py
@@ -9,3 +9,14 @@ def markdown_interpreter(file, destination):
             extensions = markdown_extensions, encoding="utf8", safe=False)
 
 interpreters["markdown"] = markdown_interpreter
+
+# Add to the list of controllers to use.
+from cytoplasm.controllers import controllerclass
+
+# the first thing in the tuple is the name of the module where the controller lives;
+# the second thing is a list of arguments to be used when instantiating a controller object.
+# In the case of the blog controller, at least, it's the source directory for it to find posts in
+# and the destination directory of its output.
+controllers = [
+    ("blog", ["_posts", "_build/blog"]),
+]
