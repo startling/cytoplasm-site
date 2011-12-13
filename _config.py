a65d6ba9ba33a8c0487f29a26ae1aa560a890671
tim <tdixon51793@gmail.com>
2011-12-13 01:24:11 -0600

    A markdown interpreter in the _config.py.

--- _config.py
+++ _config.py
@@ -1,2 +1,11 @@
 # import the defaults from cytoplasm
 from cytoplasm.defaults import *
+
+# add a markdown interpreter
+import markdown
+markdown_extensions = ['abbr', 'footnotes', 'toc', 'fenced_code', 'headerid']
+def markdown_interpreter(file, destination):
+    markdown.markdownFromFile(input=file, output=destination,
+            extensions = markdown_extensions, encoding="utf8", safe=False)
+
+interpreters["markdown"] = markdown_interpreter
