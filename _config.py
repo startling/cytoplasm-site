87c64d8151793c94d8a397f9e1c82b1fe7f3d012
tim <tdixon51793@gmail.com>
2011-12-14 12:14:38 -0600

    Specify the options for the blog controller.

--- _config.py
+++ _config.py
@@ -10,13 +10,11 @@ def markdown_interpreter(file, destination):
 
 interpreters["markdown"] = markdown_interpreter
 
-# Add to the list of controllers to use.
-from cytoplasm.controllers import controllerclass
-
 # the first thing in the tuple is the name of the module where the controller lives;
 # the second thing is a list of arguments to be used when instantiating a controller object.
 # In the case of the blog controller, at least, it's the source directory for it to find posts in
 # and the destination directory of its output.
 controllers = [
-    ("blog", ["_posts", "_build/blog"]),
+    # you can optionally specify where the templates are; here, use the controller's defaults.
+    ("blog", ["_posts", "_build/blog", "_controllers/blog/templates"]),
 ]
