95ba1e6fce55faac5e0ca6fda014500adccdf14d
tim <tdixon51793@gmail.com>
2011-12-18 19:56:27 -0600

    Add codehilite markdown extensions.
    Previously I didn't have this because it wouldn't work with the
    fenced_code extension, but I fixed that in python-markdown and now it
    should work.

--- _config.py
+++ _config.py
@@ -3,7 +3,7 @@ from cytoplasm.defaults import *
 
 # add a markdown interpreter
 import markdown
-markdown_extensions = ['abbr', 'footnotes', 'toc', 'fenced_code', 'headerid']
+markdown_extensions = ['abbr', 'footnotes', 'toc', 'fenced_code', 'headerid', 'codehilite']
 def markdown_interpreter(file, destination):
     markdown.markdownFromFile(input=file, output=destination,
             extensions = markdown_extensions, encoding="utf8", safe=False)
