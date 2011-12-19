f1b4400ad6b27fb51732a7da4fcb827768dbcace
tim <tdixon51793@gmail.com>
2011-12-18 20:38:56 -0600

    Use the new Interpreter decorator.

--- _config.py
+++ _config.py
@@ -4,20 +4,19 @@ from cytoplasm.defaults import *
 # add a markdown interpreter
 import markdown
 markdown_extensions = ['abbr', 'footnotes', 'toc', 'fenced_code', 'headerid', 'codehilite']
+
+@Interpreter("markdown", "md")
 def markdown_interpreter(file, destination):
     markdown.markdownFromFile(input=file, output=destination,
             extensions = markdown_extensions, encoding="utf8", safe=False)
 
-interpreters["markdown"] = markdown_interpreter
-
 # and a scss interpreter
 from scss import parser as scss_parser
+
+@Interpreter("scss")
 @SaveReturned
 def scss_interpreter(file):
     return scss_parser.load(file)
-
-interpreters["scss"] = scss_interpreter
-
 
 # the first thing in the tuple is the name of the module where the controller lives;
 # the second thing is a list of arguments to be used when instantiating a controller object.
